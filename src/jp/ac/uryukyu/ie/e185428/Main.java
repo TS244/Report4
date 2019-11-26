aa8cba4cee90548ee7b6377984fb215979153190
Tsuyoshi Shimoji <e185428@ie.u-ryukyu.ac.jp>
2019-11-26 13:00:54 +0900

    step1

---
+++ src/jp/ac/uryukyu/ie/e185428/Main.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e185428;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String str = null;
+        System.out.println(str.length());
+    }
+}
+
+
