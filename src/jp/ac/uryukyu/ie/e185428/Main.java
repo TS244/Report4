d979b363bd2f79749d8866b89c006c97eb1b12d1
Tsuyoshi Shimoji <e185428@ie.u-ryukyu.ac.jp>
2019-12-10 00:32:05 +0900

    step2

--- src/jp/ac/uryukyu/ie/e185428/Main.java
+++ src/jp/ac/uryukyu/ie/e185428/Main.java
@@ -3,13 +3,18 @@ package jp.ac.uryukyu.ie.e185428;
 public class Main {
 
     public static void main(String[] args) {
-	    String str = null;
-	    try {
-	        System.out.println(str.length());
-        } catch (NullPointerException e){
-	        System.out.println("NullPointerException");
-	        System.out.println(e.getMessage());
-        }
+// 	    String str = null;
+//	    try {
+//	        System.out.println(str.length());
+//	    } catch (NullPointerException e){
+//	        System.out.println("NullPointerException");
+//	        System.out.println(e.getMessage());
+//        }
+
+		String str = "百二十三";
+		int value = Integer.parseInt(str);
+
+
     }
 }
 
